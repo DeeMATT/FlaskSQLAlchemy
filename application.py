8664c5a9bb84a9f7b53a3fe103eaa85f10b3cfb2
DeeMATT <mattode@outlook.com>
2019-09-10 17:10:39 +0100

    Created Member Table in the Database

--- application.py
+++ application.py
@@ -8,10 +8,16 @@ db = SQLAlchemy(app)
 
 class Test(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-
+ 
 class Member(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True)
+    password = db.Column(db.String(30))
+    email = db.Column(db.String(50))
+    join_date = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return '<Member %r>' % self.username
 
 if __name__ == "__main__":
     app.run()
